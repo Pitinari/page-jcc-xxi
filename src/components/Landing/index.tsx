721d98b8f695880a34092b18640f353bca267cfe
Pitinari <tomy.pitinari@gmail.com>
2023-08-14 23:29:15 -0300

    Feat(Landing): random characters loops

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { LandingProps } from './types';
 import classNames from 'classnames';
 import "./styles.scss"
@@ -9,6 +9,12 @@ export const Landing: React.FC<LandingProps> = ({ className }) => {
         window.innerWidth,
         window.innerHeight,
     ]);
+    const intervals = useRef(Array(9).fill(0));
+    // const [tape, setTape] = useState(
+    //     Array(9).fill("").map(_ => String.fromCharCode(Math.random() * 93 + 33)) 
+    // );
+    // console.log(tape);
+    
 
     useEffect(() => {
         const handleWindowResize = () => {
@@ -17,8 +23,19 @@ export const Landing: React.FC<LandingProps> = ({ className }) => {
 
         window.addEventListener('resize', handleWindowResize);
 
+        intervals.current.forEach((_, idx) => {
+            intervals.current[idx] = setInterval(() => {
+                const character = document.getElementById(`char__${idx}`);
+                if(character){
+                    character.replaceChildren(String.fromCharCode(Math.random() * 93 + 33));
+                }          
+                
+            }, 300 + Math.random() * 50)
+        })
+
         return () => {
             window.removeEventListener('resize', handleWindowResize);
+            intervals.current.forEach(int => clearInterval(int))
         };
     }, []);
 
@@ -29,10 +46,12 @@ export const Landing: React.FC<LandingProps> = ({ className }) => {
 
     return (<div className={classNames(className, 'landing', 'relative h-full w-full')}>
         <div className={'landing__tape'} style={{ height: boxHeight }}>
-            {Array(10).fill("").map((_, idx) => (
-                <div className='landing__box' style={{ height: boxInnerHeight, width: boxInnerWidth, transform: `translate(${(boxWidth * idx) - boxWidth/2}px,-50%)`  }} key={idx}/>
+            {Array(9).fill("").map((char, idx) => (
+                <div className='landing__box' style={{ height: boxInnerHeight, width: boxInnerWidth, transform: `translate(${(boxWidth * idx) - boxWidth/2}px,-50%)`  }} key={idx}>
+                    <h1 className='landing__box__character' style={{ fontSize: boxInnerHeight -20 }} id={`char__${idx}`}>{char}</h1>
+                </div>
             ))}
         </div>
-        <div className='landing__pointer' style={{ transform: `translate(${(boxWidth-20)}px,-${boxHeight/2 + 24}px)` }}></div>
+        <div className='landing__pointer' style={{ transform: `translate(${boxWidth -25}px,-${boxHeight/2 + 24}px)` }}></div>
     </div>)
 }
